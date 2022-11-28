b3053cae65d1477f0e25f543b1329c47e5b6581a
akshata_rn <akshatarn123@gamail.com>
2022-11-28 13:22:00 +0530

    Refactored to one generic method and found maximum

--- MaximumGenerics/Program.cs
+++ MaximumGenerics/Program.cs
@@ -10,10 +10,26 @@ namespace MaximumGenerics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Generics Find Maximum Program");
             Console.WriteLine("Welcome to Generic Use Case's ");
-            FindMax.FindMaxValue(22.34f, 30.45f, 40.56f); // Finding maximum of 3 float numbers
-            
+            Console.WriteLine("Choose any one from below options");
+            Console.WriteLine("1:Find Max Num\n" +
+                "2:FindMax using Generic Method");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FindMax.FindMaxValue(20, 30, 40);                   
+                    FindMax.FindMaxValue(20.20f, 30.20f, 40.40f);       
+                    FindMax.FindMaxValue("abc", "def", "ghi");       
+                    break;
+                case 2:
+                    GenericFindMax<int>.FindMaxValue(11, 23, 30);       //find max using generic method
+                    break;
+                default:
+                    Console.WriteLine("Please choose number within given range!");
+                    break;
+            }
         }
     }
 }
